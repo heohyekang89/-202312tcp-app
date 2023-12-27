ca01277e70009782849ae13abb20146019b6cfb4
HyeKangHeo <hyekang89@gmail.com>
2023-12-27 14:54:11 +0900

    add Logging.cs
    폴더 이름 ignore로 인한 누락

---
+++ Core/nLog/Logging.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+
+namespace RandomApp;
+
+public abstract class Logging
+{
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    public static void Info(dynamic? input)
+    {
+        Logger.Info(input);
+    }
+    
+    public static void Error(dynamic? input)
+    {
+        Logger.Error(input);
+    }
+}
